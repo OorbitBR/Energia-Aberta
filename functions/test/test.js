b251976e2a5510a6e7cff0073e42188ef9063211
shabarito <equipeoorbit@gmail.com>
2023-10-04 23:23:29 -0300

    Main
    Main

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,10 +1,16 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
+
+  const url = 'https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=ae00cc29-5c12-4da5-970f-ec1f5a251fe0';
+
+  const response = await fetch(url);
+
+  console.log(response);
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: response }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
