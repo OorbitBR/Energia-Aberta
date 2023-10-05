ad51a8339f6b4442a0e0871f1845b6339c3fe298
shabarito <equipeoorbit@gmail.com>
2023-10-04 23:30:30 -0300

    Update test.js

--- functions/test/test.js
+++ functions/test/test.js
@@ -2,15 +2,14 @@
 const handler = async (event) => {
 
   const url = 'https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=ae00cc29-5c12-4da5-970f-ec1f5a251fe0';
-
-  const response = await fetch(url);
-
-  console.log(response);
-
+  
   try {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
     return {
-      statusCode: 201,
-      body: JSON.stringify({ message: response }),
+      statusCode: 200,
+      body: JSON.stringify({ message: xmlHttp.responseText }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
