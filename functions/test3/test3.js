670422ff68f2a361848bef8065952afbedf644c3
shabarito <equipeoorbit@gmail.com>
2023-10-04 23:41:54 -0300

    Update test3.js

--- functions/test3/test3.js
+++ functions/test3/test3.js
@@ -1,5 +1,7 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+/*
 const handler = async (event) => {
+    const url = 'https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=ae00cc29-5c12-4da5-970f-ec1f5a251fe0';
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url, true ); // false for synchronous request
     xmlHttp.send( null );
@@ -13,3 +15,52 @@ const handler = async (event) => {
 }
 
 module.exports = { handler }
+*/
+
+const https = require('https');
+
+function getRequest() {
+  const url = 'https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=ae00cc29-5c12-4da5-970f-ec1f5a251fe0';
+
+  return new Promise((resolve, reject) => {
+    const req = https.get(url, res => {
+      let rawData = '';
+
+      res.on('data', chunk => {
+        rawData += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(rawData));
+        } catch (err) {
+          reject(new Error(err));
+        }
+      });
+    });
+
+    req.on('error', err => {
+      reject(new Error(err));
+    });
+  });
+}
+
+exports.handler = async event => {
+  try {
+    const result = await getRequest();
+    console.log('result is: 👉️', result);
+
+    // 👇️️ response structure assume you use proxy integration with API gateway
+    return {
+      statusCode: 200,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    console.log('Error is: 👉️', error);
+    return {
+      statusCode: 400,
+      body: error.message,
+    };
+  }
+};
