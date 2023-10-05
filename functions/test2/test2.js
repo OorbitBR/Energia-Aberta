1f543b1992abd48820cb70870195ca8465218508
shabarito <equipeoorbit@gmail.com>
2023-10-04 23:36:25 -0300

    Main

--- functions/test2/test2.js
+++ functions/test2/test2.js
@@ -1,16 +1,10 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-
-  const url = 'https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=ae00cc29-5c12-4da5-970f-ec1f5a251fe0';
-
   try {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
-    xmlHttp.send( null );
-    
+    const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: xmlHttp.responseText }),
+      body: JSON.stringify({ message: `Hello ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
--- functions/test3/test3.js
+++ functions/test3/test3.js
@@ -1,13 +1,8 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-
-  const url = 'https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=ae00cc29-5c12-4da5-970f-ec1f5a251fe0';
-
-  try {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.open( "GET", url, true ); // false for synchronous request
     xmlHttp.send( null );
-    
     return {
       statusCode: 200,
       body: JSON.stringify({ message: xmlHttp.responseText }),
@@ -15,9 +10,6 @@ const handler = async (event) => {
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
 
 module.exports = { handler }
